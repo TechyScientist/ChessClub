ab0647157c33a78434501b7ec94e211b9b5fbf34
Johnny Console <johnny.console@hotmail.com>
2026-03-13 12:38:53 -0400

    Add unlisted servlet to get the UserDao object reference

---
+++ Base/chessclub-web/src/main/java/GetUserDaoServlet.java
@@ -0,0 +1,22 @@
+import com.johnnyconsole.chessclub.persistence.interfaces.UserDao;
+
+import javax.ejb.EJB;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("GetUserDaoServlet")
+public class GetUserDaoServlet extends HttpServlet {
+
+    @EJB
+    private UserDao userDao;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.getSession().setAttribute("UserDao", userDao);
+        response.sendRedirect(request.getHeader("Referer"));
+    }
+
+}
