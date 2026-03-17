98e014e82589add63fa3035cb43022b37792b6b7
Johnny Console <johnny.console@hotmail.com>
2026-03-17 12:59:35 -0400

    Add named query to fetch all games played by a user

--- Base/chessclub-ejb/src/main/java/com/johnnyconsole/chessclub/persistence/User.java
+++ Base/chessclub-ejb/src/main/java/com/johnnyconsole/chessclub/persistence/User.java
@@ -13,7 +13,8 @@ import java.time.LocalDate;
         @NamedQuery(name="User.FindByUsername", query="SELECT u FROM User u WHERE u.username = :username"),
         @NamedQuery(name="User.FindAllExcept", query="SELECT u FROM User u WHERE u.id <> :id"),
         @NamedQuery(name="User.Count", query="SELECT COUNT(u) AS count FROM User u"),
-        @NamedQuery(name="User.GamesPlayed", query="SELECT COUNT(g.id) AS count FROM Game g WHERE g.whitePlayer = :id OR g.blackPlayer = :id")
+        @NamedQuery(name="User.GamesPlayed", query="SELECT COUNT(g.id) AS count FROM Game g WHERE g.whitePlayer = :id OR g.blackPlayer = :id"),
+        @NamedQuery(name="User.FindGames", query="SELECT g FROM Game g WHERE g.whitePlayer = :id OR g.blackPlayer = :id ORDER BY g.datePlayed DESC"),
 })
 public class User {
 
