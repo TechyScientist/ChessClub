c26f07c43ca8ddfc0acf9f9bdae114902b4b7df5
Johnny Console <johnny.console@hotmail.com>
2026-03-17 15:48:44 -0400

    Allow users to sign out

---
+++ Base/chessclub-web/src/main/java/com/johnnyconsole/chessclub/servlet/SignOutServlet.java
@@ -0,0 +1,19 @@
+package com.johnnyconsole.chessclub.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet("/SignOutServlet")
+public class SignOutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.getSession().removeAttribute("SignedInUser");
+        request.getSession().setAttribute("status", HttpServletResponse.SC_ACCEPTED);
+        response.sendRedirect("/");
+    }
+}
