2737e119c1f01ca947d868f347ec29500501a364
Johnny Console <johnny.console@hotmail.com>
2026-03-18 07:59:38 -0400

    Rename GetUserDaoServlet to GetDaosServlet for future use

--- Base/chessclub-web/src/main/java/com/johnnyconsole/chessclub/servlet/GetDaosServlet.java
+++ Base/chessclub-web/src/main/java/com/johnnyconsole/chessclub/servlet/GetDaosServlet.java
@@ -9,8 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("GetUserDaoServlet")
-public class GetUserDaoServlet extends HttpServlet {
+@WebServlet("GetDaosServlet")
+public class GetDaosServlet extends HttpServlet {
 
     @EJB
     private UserDao userDao;
